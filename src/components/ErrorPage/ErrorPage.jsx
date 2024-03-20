a5e1fcce90a9b2f000a48a9e53e6070bbc5ab0dc
SohelDewan <sohelds241@gmail.com>
2024-03-20 23:13:36 +0600

    useParam

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,12 +1,18 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
     const error = useRouteError();
-    console.log(error);
+    // console.log(error);
     return (
         <div>
             <h2>Opps!!!</h2>
             <p>{error.statusText || error.message}</p>
+            {
+                error.status === 404 && <div>
+                    <h3>Page Not Found</h3>
+                    <Link to='/'> <button>Go Back to Home</button></Link>
+                </div>
+            }
         </div>
     );
 };
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,15 +1,19 @@
 import { Link, useNavigate } from "react-router-dom";
+
 const Post = ({post}) => {
+
     const {id, title} = post;
     const postStyle ={
         border: '2px solid green',
         padding: '20px',
         borderRadius: '10px'
     }
+
     const navigate = useNavigate();
     const handleDetail = ()=>{
         navigate(`/post/${id}`);
     }
+    
     return (
         <div style={postStyle}>
             <h3>Post: {id}</h3>
